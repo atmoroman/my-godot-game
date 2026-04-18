d4c611bcc2476400ab0e46928422d5cda1ba0235
Atmoroman <atmoroma@gmail.com>
2026-04-18 23:17:08 +0100

    Created bind for chords

--- project/guitar.gd
+++ project/guitar.gd
@@ -6,3 +6,15 @@ extends Node2D
 	"C":  $C,
 	"F":  $F
 }
+
+@onready var strum_players = [$StrumA, $StrumB]
+@onready var chord_label   = $ChordLabel
+@onready var pattern_btn   = $PatternButton
+
+# Map keyboard keys → chord names
+var key_to_chord = {
+	KEY_A: "Am",
+	KEY_S: "G",
+	KEY_D: "C",
+	KEY_F: "F"
+}
